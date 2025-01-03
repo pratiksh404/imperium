8259585cf87b02ca1227fc5ebf6b89572e82238d
pratiksh404 <pratikdai404@gmail.com>
2025-01-03 12:20:52 +0545

    default presistant layout setup

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,23 +1,11 @@
 <script setup>
 import { ref, useTemplateRef, onMounted } from 'vue';
 import { usePage } from '@inertiajs/vue3';
-import LinksBreadcrumb from '@/Components/PrimeVue/LinksBreadcrumb.vue';
 import MobileSidebarNavDrawer from './Partials/MobileSidebarNavDrawer.vue';
 import TopNav from './Partials/TopNav.vue';
 import Footer from './Partials/Footer.vue';
 import SideMenuItems from './Partials/SideMenuItems.vue';
 
-const props = defineProps({
-    pageTitle: {
-        type: String,
-        required: false,
-    },
-    breadcrumbs: {
-        type: Array,
-        required: false,
-        default: () => [],
-    },
-});
 
 const page = usePage();
 
@@ -43,83 +31,29 @@ onMounted(() => {
     </Teleport>
 
     <div class="h-screen flex flex-col">
-        <header
-            id="site-header"
-            ref="site-header"
-            class="block lg:fixed top-0 left-0 right-0 z-50"
-        >
+        <header id="site-header" ref="site-header" class="block lg:fixed top-0 left-0 right-0 z-50">
             <!-- Main Nav -->
             <TopNav @open-nav="navDrawerOpen = true" />
         </header>
 
         <main class="flex-1">
             <!-- Desktop Sidebar -->
-            <aside
-                :class="[
-                    'w-[18rem] inset-0 hidden lg:block fixed overflow-y-auto overflow-x-hidden dynamic-bg border-r dynamic-border',
-                    `top-[${headerHeight}]`,
-                ]"
-            >
+            <aside :class="[
+                'w-[18rem] inset-0 hidden lg:block fixed overflow-y-auto overflow-x-hidden dynamic-bg border-r dynamic-border',
+                `top-[${headerHeight}]`,
+            ]">
                 <div class="w-full px-8 py-6">
                     <SideMenuItems />
                 </div>
             </aside>
 
             <!-- Scrollable Content -->
-            <div
-                :class="[
-                    'flex flex-col h-full lg:pl-[18rem]',
-                    `lg:pt-[${headerHeight}]`,
-                ]"
-            >
-                <!-- Breadcrumbs Nav -->
-                <nav
-                    v-if="breadcrumbs.length"
-                    class="dynamic-bg border-b dynamic-border"
-                >
-                    <Container :fluid="true">
-                        <div
-                            class="flex items-center justify-between flex-wrap"
-                        >
-                            <div>
-                                <LinksBreadcrumb
-                                    :model="breadcrumbs"
-                                    class="py-3 lg:py-5"
-                                />
-                            </div>
-                            <div>
-                                <div v-if="$slots.headerEnd" class="py-3">
-                                    <slot name="headerEnd" />
-                                </div>
-                            </div>
-                        </div>
-                    </Container>
-                </nav>
-
-                <!-- Page Title -->
-                <section v-if="pageTitle">
-                    <Container :fluid="true" class="my-4 md:mt-8 md:mb-6">
-                        <div class="flex items-end justify-between flex-wrap">
-                            <div>
-                                <h1
-                                    class="font-bold text-2xl md:text-3xl leading-tight"
-                                >
-                                    {{ pageTitle }}
-                                </h1>
-                            </div>
-                            <div>
-                                <div v-if="$slots.titleEnd">
-                                    <slot name="titleEnd" />
-                                </div>
-                            </div>
-                        </div>
-                    </Container>
-                </section>
-
+            <div :class="[
+                'flex flex-col h-full lg:pl-[18rem]',
+                `lg:pt-[${headerHeight}]`,
+            ]">
                 <!-- Page Content -->
-                <section id="page-content" class="grow">
-                    <slot />
-                </section>
+                <slot />
 
                 <footer class="">
                     <Footer />
---
+++ resources/js/Layouts/ContentLayout.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- Breadcrumbs Nav -->
+    <nav v-if="breadcrumbs.length" class="dynamic-bg border-b dynamic-border">
+        <Container :fluid="true">
+            <div class="flex items-center justify-between flex-wrap">
+                <div>
+                    <LinksBreadcrumb :model="breadcrumbs" class="py-3 lg:py-5" />
+                </div>
+                <div>
+                    <div v-if="$slots.headerEnd" class="py-3">
+                        <slot name="headerEnd" />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    </nav>
+
+    <!-- Page Title -->
+    <section v-if="pageTitle">
+        <Container :fluid="true" class="my-4 md:mt-8 md:mb-6">
+            <div class="flex items-end justify-between flex-wrap">
+                <div>
+                    <h1 class="font-bold text-2xl md:text-3xl leading-tight">
+                        {{ pageTitle }}
+                    </h1>
+                </div>
+                <div>
+                    <div v-if="$slots.titleEnd">
+                        <slot name="titleEnd" />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    </section>
+
+    <!-- Page Content -->
+    <section id="page-content" class="grow">
+        <slot />
+    </section>
+</template>
+<script setup>
+import LinksBreadcrumb from '@/Components/PrimeVue/LinksBreadcrumb.vue';
+const props = defineProps({
+    pageTitle: {
+        type: String,
+        required: false,
+    },
+    breadcrumbs: {
+        type: Array,
+        required: false,
+        default: () => [],
+    },
+});
+console.log(props);
+</script>
