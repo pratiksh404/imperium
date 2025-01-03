f57b5e9cf7063f4d8d09d4708e1d262be069ec15
pratiksh404 <pratikdai404@gmail.com>
2025-01-03 14:37:21 +0545

    file management

--- resources/js/Layouts/Partials/Footer.vue
+++ resources/js/Layouts/Partials/Footer.vue
@@ -1,9 +1,7 @@
 <template>
-    <div class="pt-4 md:pt-8">
+    <div class="pt-2 md:pt-8">
         <div class="w-full border-t dynamic-border">
-            <div
-                class="flex flex-col items-center justify-center text-center dynamic-bg py-6 px-3"
-            >
+            <div class="flex flex-col items-center justify-center text-center dynamic-bg py-3 px-3">
                 <p>Your Company - {{ new Date().getFullYear() }}</p>
             </div>
         </div>
