7f862f2caa589df6ed06612080f51345a4738da9
pratiksh404 <pratikdai404@gmail.com>
2025-01-11 17:51:03 +0545

    active menu green doot indicator

--- resources/js/Components/Navigation/MenuLinkItem.vue
+++ resources/js/Components/Navigation/MenuLinkItem.vue
@@ -2,9 +2,11 @@
     <!-- Menu Link Item -->
     <Link :href="item.url" v-ripple
         class="flex items-center justify-between cursor-pointer px-4 py-3 rounded text-surface-700 0 dark:text-surface-0 no-underline  duration-150 transition-colors p-ripple">
-    <div>
+    <div class="flex items-start">
         <i :class="item.icon" class="mr-2"></i>
         <span :class="active ? 'font-bold' : 'font-medium'">{{ item.label }}</span>
+        <!-- Green Dot if active -->
+        <span v-if="active" class="flex w-2 h-2 ml-1  bg-green-500 rounded-full"></span>
     </div>
     <span v-if="item.badge"
         class="inline-flex items-center justify-center ml-2 bg-primary text-primary-contrast rounded-full "
